b948fed58bbac17918b4ec0392a941c2f8127760
Soskiiit <nikitaa439@gmail.com>
2025-02-15 01:41:52 +0300

    lab_1 initial commit

---
+++ main.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+#include "mymath/mymath.h"
+
+int main() {
+    setlocale(LC_ALL, "Russian");
+    std::cout << "Я умею суммировать! Зацени 10+32=";
+    std::cout << mymath::sum(10, 32) << std::endl;
+    return 0;
+}
