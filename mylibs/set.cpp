be915b487a8a1ef1112efc7669ba3a357ed2e2c5
Soskiiit <nikitaa439@gmail.com>
2025-03-13 13:51:50 +0300

    lab5 v1.0

---
+++ mylibs/set.cpp
@@ -0,0 +1,8 @@
+#include "set.h"
+
+
+template class set::Set<int>;
+template class set::Set<long>;
+template class set::Set<float>;
+template class set::Set<double>;
+template class set::Set<char>;
---
+++ mylibs/vector.cpp
@@ -0,0 +1,7 @@
+#include "vector.h"
+
+template class vector::Vector<int>;
+template class vector::Vector<long>;
+template class vector::Vector<float>;
+template class vector::Vector<double>;
+template class vector::Vector<char>;
